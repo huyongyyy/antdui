ff5c47c3153066baec641f03d56df10fe632247f
huyong456 <1065975066@qq.com>
2023-03-16 17:13:21 +0800

    文档更新

---
+++ assets/docs_quickstart.md.06cfdda8.js
@@ -0,0 +1,19 @@
+import{_ as s,o as n,c as a,g as p}from"./app.e2fb92b7.js";const h=JSON.parse('{"title":"\u5FEB\u901F\u5F00\u59CB","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7528\u6CD5","slug":"\u7528\u6CD5","link":"#\u7528\u6CD5","children":[]},{"level":2,"title":"\u5168\u5C40\u5F15\u5165","slug":"\u5168\u5C40\u5F15\u5165","link":"#\u5168\u5C40\u5F15\u5165","children":[]}],"relativePath":"docs/quickstart.md","lastUpdated":1678690948000}'),e={name:"docs/quickstart.md"},t=p(`<h1 id="\u5FEB\u901F\u5F00\u59CB" tabindex="-1">\u5FEB\u901F\u5F00\u59CB <a class="header-anchor" href="#\u5FEB\u901F\u5F00\u59CB" aria-hidden="true">#</a></h1><p>\u672C\u8282\u5C06\u4ECB\u7ECD\u5982\u4F55\u5728\u9879\u76EE\u4E2D\u4F7F\u7528 AntComponentV3</p><p><strong>\u63D0\u793A\uFF1A</strong> \u8BE5\u7EC4\u4EF6\u57FA\u4E8EAnt Design Vue\u4E8C\u6B21\u5F00\u53D1\uFF0C\u4F7F\u7528\u524D\u8BF7\u5148\u5B89\u88C5Ant Design Vue 3.x+\u3002</p><h2 id="\u7528\u6CD5" tabindex="-1">\u7528\u6CD5 <a class="header-anchor" href="#\u7528\u6CD5" aria-hidden="true">#</a></h2><div class="language-"><button class="copy"></button><span class="lang"></span><pre><code><span class="line"><span style="color:#A6ACCD;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">  &lt;TableSearch&gt;\u6309\u94AE&lt;/TableSearch&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;script setup&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">import { TableSearch } from &#39;ant-component-v3&#39;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/script&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="\u5168\u5C40\u5F15\u5165" tabindex="-1">\u5168\u5C40\u5F15\u5165 <a class="header-anchor" href="#\u5168\u5C40\u5F15\u5165" aria-hidden="true">#</a></h2><div class="language-"><button class="copy"></button><span class="lang"></span><pre><code><span class="line"><span style="color:#A6ACCD;">import { createApp } from &#39;vue&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">import Antd from &#39;ant-design-vue&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">import App from &#39;./App&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">import &#39;ant-design-vue/dist/antd.css&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">import AntCprt from &quot;ant-component-v3&quot;;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">const app = createApp(App);</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">app.use(Antd).use(AntCprt).mount(&#39;#app&#39;);</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,7),l=[t];function o(c,r,i,A,d,C){return n(),a("div",null,l)}const _=s(e,[["render",o]]);export{h as __pageData,_ as default};
